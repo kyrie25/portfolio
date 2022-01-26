77b140de4ff3346644441668ca2bee6a84434a84
Nam Anh <phamnamanh25@gmail.com>
2022-01-26 22:14:21 +0700

    about page

--- js/terminal.js
+++ js/terminal.js
@@ -53,6 +53,9 @@ document.addEventListener(
             case "music":
               music();
               break;
+            case "about":
+              about();
+              break;
             case "clear":
               clear();
               break;
@@ -167,6 +170,15 @@ function printmusic() {
   switchToNewLine();
 }
 
+function printabout() {
+  const j = document.querySelector(".new-line");
+  j.classList.remove("new-line");
+  if (j.innerHTML.endsWith("|")) j.innerHTML = j.innerHTML.slice(0, -1);
+  j.innerHTML += "about";
+  about();
+  switchToNewLine();
+}
+
 function print404() {
   const j = document.querySelector(".new-line");
   j.classList.remove("new-line");
@@ -222,5 +234,24 @@ function music() {
     "  <div><a href=\"https://www.youtube.com/channel/UC10BM9XdLdrvB8japwmRUvA\" target='_blank'>Kanaria</a></div>\n" +
     "  <div><a href=\"https://www.youtube.com/c/inabakumori\" target='_blank'>inabakumori</a></div>\n" +
     "  <div>...and a ton others but too lazy to label, just click the playlist at the top ig</div>\n" +
+    '  <div style="padding-bottom:10px; clear: both;">';
+}
+
+function about() {
+  terminal.innerHTML +=
+    '\n<img alt="icon" class="img" src="static/about.png">\n' +
+    "  <div><span>kyrie25</span>@<span>github.io</span></div>\n" +
+    "  <div>---------------------</div>\n" +
+    "  <div><span>About this page</span></div>\n" +
+    "  <div>This is my <span>first ever</span> website\n" +
+    "  <div>Basically a portfolio/'About Me'\n" +
+    "  <div>It probably won't be updated at the time you're reading this</div>\n" +
+    "  <div>Credit to stuff I've used in this page:</div>\n" +
+    "  <div>About 70% of the website: Me\n" +
+    "  <div><span>Core script/functionality</span>: Generous stranger on GitHub, not sure who they were\n" +
+    "  <div><span>Nazrin</span>: rrat\n" +
+    "  <div><span>Home page artwork</span>: <a href=\"https://www.pixiv.net/en/artworks/58056024\" target='_blank'>にしもん</a></div>\n" +
+    "  <div><span>Music page artwork</span>: <a href=\"https://twitter.com/ramdayo1122/status/1480119998391918594\" target='_blank'>LAM</a></div>\n" +
+    "  <div>Please support the respective owners of the works, they really are awesome <3\n" +
     '  <div style="padding-bottom:10px; clear: both;">';
 }
