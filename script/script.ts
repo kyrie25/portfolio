f7fe5a8eaf85551187854cdc3a0f9999800f163c
Nam Anh <phamnamanh25@gmail.com>
2022-02-26 12:45:43 +0700

    🐛

--- script/script.ts
+++ script/script.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-template */
 setInterval(
 	(function a() {
 		const [b, c] = [new Date(), new Date()];
@@ -5,25 +6,20 @@ setInterval(
 		c.setHours(0, 0, 0);
 		c.setDate(c.getDate() + 1);
 
-		document.querySelector<HTMLDivElement>(
-			".date-widget"
-		).innerHTML = `<svg class="date-display__icon" height="24" viewBox="0 0 24 24" width="24"><path d="M21 3h-3V1h-2v2H8V1H6v2H3v18h18V3zm-2 16H5V8h14v11zM7 10h5v5H7v-5z"></path></svg>${new Date().toLocaleString(
-			"en-UK",
-			{
+		document.querySelector<HTMLDivElement>(".date-widget").innerHTML =
+			'<svg class="date-display__icon" height="24" viewBox="0 0 24 24" width="24"><path d="M21 3h-3V1h-2v2H8V1H6v2H3v18h18V3zm-2 16H5V8h14v11zM7 10h5v5H7v-5z"></path></svg>' +
+			new Date().toLocaleString("en-UK", {
 				weekday: "short",
 				month: "short",
 				day: "numeric"
-			}
-		)}`;
-		document.querySelector<HTMLDivElement>(
-			".time-widget"
-		).innerHTML = `<svg class="time__icon" height="24" viewBox="0 0 24 24" width="24"><path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm.5-13H11v6l5.25 3.15.75-1.23-4.5-2.67z"></path></svg>${new Date().toLocaleString(
-			"en-UK",
-			{
+			});
+		document.querySelector<HTMLDivElement>(".time-widget").innerHTML =
+			'<svg class="time__icon" height="24" viewBox="0 0 24 24" width="24"><path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm.5-13H11v6l5.25 3.15.75-1.23-4.5-2.67z"></path></svg>' +
+			new Date().toLocaleString("en-UK", {
 				hour: "numeric",
 				minute: "numeric"
-			}
-		)}<div class="time__filler"></div>`;
+			}) +
+			'<div class="time__filler"></div>';
 		document.querySelector<HTMLDivElement>(
 			".time__filler"
 		).style.transform = `scaleX(${
@@ -61,8 +57,8 @@ function onMouseMuse(h: MouseEvent) {
 	pos2 = pos4 - h.clientY;
 	pos3 = h.clientX;
 	pos4 = h.clientY;
-	elem.style.top = `${elem.offsetTop - pos2}px`;
-	elem.style.left = `${elem.offsetLeft - pos1}px`;
+	elem.style.top = elem.offsetTop - pos2 + "px";
+	elem.style.left = elem.offsetLeft - pos1 + "px";
 	checkPos();
 }
 function onMouseUp() {
@@ -102,16 +98,16 @@ document.addEventListener(
 		if (l.altKey) {
 			switch (l.key) {
 				case "ArrowUp":
-					elem.style.top = `${elem.offsetTop - 20}px`;
+					elem.style.top = elem.offsetTop - 20 + "px";
 					break;
 				case "ArrowDown":
-					elem.style.top = `${elem.offsetTop + 20}px`;
+					elem.style.top = elem.offsetTop + 20 + "px";
 					break;
 				case "ArrowLeft":
-					elem.style.left = `${elem.offsetLeft - 20}px`;
+					elem.style.left = elem.offsetLeft - 20 + "px";
 					break;
 				case "ArrowRight":
-					elem.style.left = `${elem.offsetLeft + 20}px`;
+					elem.style.left = elem.offsetLeft + 20 + "px";
 					break;
 			}
 			checkPos();
@@ -121,11 +117,11 @@ document.addEventListener(
 );
 function checkPos() {
 	if (elem.offsetHeight + elem.offsetTop > window.innerHeight)
-		elem.style.top = `${window.innerHeight - elem.offsetHeight}px`;
+		elem.style.top = window.innerHeight - elem.offsetHeight + "px";
 	if (elem.offsetWidth + elem.offsetLeft > window.innerWidth)
-		elem.style.left = `${window.innerWidth - elem.offsetWidth}px`;
+		elem.style.left = window.innerWidth - elem.offsetWidth + "px";
 	if (elem.offsetLeft < -elem.offsetWidth / 2)
-		elem.style.left = `${-Math.floor(elem.offsetWidth / 2)}px`;
+		elem.style.left = -Math.floor(elem.offsetWidth / 2) + "px";
 	if (elem.offsetTop < -elem.offsetHeight / 2)
-		elem.style.top = `${-Math.floor(elem.offsetHeight / 2)}px`;
+		elem.style.top = -Math.floor(elem.offsetHeight / 2) + "px";
 }
