b506dfc53b12822dc0a16df3df974f5dce91781d
Nam Anh <phamnamanh25@gmail.com>
2023-02-07 19:20:29 +0700

    fix(Dock): image position on hover

--- src/components/Dock/index.tsx
+++ src/components/Dock/index.tsx
@@ -1,4 +1,10 @@
-import React, { useEffect } from "react";
+import React, {
+	DetailedHTMLProps,
+	ImgHTMLAttributes,
+	ReactEventHandler,
+	SyntheticEvent,
+	useEffect
+} from "react";
 import "./Dock.scss";
 
 import nowPlaying from "assets/now_playing.gif";
@@ -15,7 +21,10 @@ const Dock = () => {
 	const { data, revalidate } = useLanyard(import.meta.env.VITE_DISCORD_ID);
 
 	useEffect(() => {
-		revalidate();
+		const refresh = setInterval(() => {
+			revalidate();
+			return () => clearInterval(refresh);
+		}, 5000);
 	}, []);
 
 	if (!data?.spotify) {
@@ -26,11 +35,38 @@ const Dock = () => {
 		);
 	}
 
+	/**
+	 * Function to align album image cover perfectly on the left edge of the viewport on hover.
+	 *
+	 * Since the image height is always gonna be larger than 50px, only width is needed.
+	 */
+	const getImageLocation = (image: SyntheticEvent<HTMLImageElement, Event>) => {
+		const { x, width } = image.currentTarget.getBoundingClientRect();
+
+		/** Image is scaled up by 5x, so we need to divide the width by 5 to get the original width.
+		 * The scaled width is then subtracted from the original width to get the difference.
+		 * Then we need to divide the original width by 2 to get the difference on only one side.
+		 */
+		const incrementedWidth = (width * 5 - width) / 2;
+
+		/** Since this only needs to work on small screens, if the width is smaller than x, we can just set it to 0.
+		 * idk why we need to divide by 5, but it works.
+		 */
+		const desiredX =
+			x - incrementedWidth > 0 ? 0 : Math.abs(x - incrementedWidth) / 5;
+
+		image.currentTarget.style.setProperty("--x", `${desiredX}px`);
+	};
+
 	const { spotify } = data;
 
 	return (
 		<footer className="dock">
-			<img alt={spotify.album} src={spotify.album_art_url || nowPlaying} />
+			<img
+				alt={spotify.album}
+				src={spotify.album_art_url || nowPlaying}
+				onLoad={getImageLocation}
+			/>
 			<p>
 				Listening to:&nbsp;
 				<a href={`https://open.spotify.com/track/${spotify.track_id}`}>
