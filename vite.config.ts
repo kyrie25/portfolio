8963c3b70a018f5e9bca76033367c89e3e94b8d1
Nam Anh <phamnamanh25@gmail.com>
2023-03-27 06:22:14 +0700

    feat: compress image

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import svgrPlugin from "vite-plugin-svgr";
+import viteImagemin from "vite-plugin-imagemin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,5 +15,36 @@ export default defineConfig({
 			cert: "./ssl/domain.cert.pem"
 		}
 	},
-	plugins: [react(), viteTsconfigPaths(), svgrPlugin()]
+	plugins: [
+		react(),
+		viteTsconfigPaths(),
+		svgrPlugin(),
+		viteImagemin({
+			gifsicle: {
+				optimizationLevel: 7,
+				interlaced: false
+			},
+			optipng: {
+				optimizationLevel: 7
+			},
+			mozjpeg: {
+				quality: 20
+			},
+			pngquant: {
+				quality: [0.8, 0.9],
+				speed: 4
+			},
+			svgo: {
+				plugins: [
+					{
+						name: "removeViewBox"
+					},
+					{
+						name: "removeEmptyAttrs",
+						active: false
+					}
+				]
+			}
+		})
+	]
 });
