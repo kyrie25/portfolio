404213ecdc7ebdcbcd75cbd4fd1d4360c1fb1745
Nam Anh <phamnamanh25@gmail.com>
2023-06-24 10:43:12 +0700

    chore: remove dep

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import svgrPlugin from "vite-plugin-svgr";
-import viteImagemin from "vite-plugin-imagemin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,36 +14,5 @@ export default defineConfig({
 			cert: "./ssl/domain.cert.pem"
 		}
 	},
-	plugins: [
-		react(),
-		viteTsconfigPaths(),
-		svgrPlugin(),
-		viteImagemin({
-			gifsicle: {
-				optimizationLevel: 7,
-				interlaced: false
-			},
-			optipng: {
-				optimizationLevel: 7
-			},
-			mozjpeg: {
-				quality: 20
-			},
-			pngquant: {
-				quality: [0.8, 0.9],
-				speed: 4
-			},
-			svgo: {
-				plugins: [
-					{
-						name: "removeViewBox"
-					},
-					{
-						name: "removeEmptyAttrs",
-						active: false
-					}
-				]
-			}
-		})
-	]
+	plugins: [react(), viteTsconfigPaths(), svgrPlugin()]
 });
