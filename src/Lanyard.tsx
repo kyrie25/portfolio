d3603194d124a99475f6fa200b9899444b3cf8d2
Nam Anh <phamnamanh25@gmail.com>
2024-04-22 12:16:11 +0700

    refactor: cleanup

--- src/Lanyard.tsx
+++ src/Lanyard.tsx
@@ -72,31 +72,3 @@ export const Lanyard = ({ id }) => {
 		</>
 	);
 };
-
-const formatTime = (timestamps?: { start: number; end?: number }) => {
-	if (!timestamps) return;
-	const { start, end } = timestamps;
-	if (!start && !end) return;
-	// End timestamps is prioritized over start timestamps and displayed accordingly.
-	const startTime = new Date(end || start).getTime();
-	const endTime = Date.now();
-	let difference = end ? (startTime - endTime) / 1000 : (endTime - startTime) / 1000;
-	if (difference < 0) return `00:00 ${end ? "left" : "elapsed"}`;
-
-	// we only calculate them, but we don't display them.
-	// this fixes a bug in the Discord API that does not send the correct timestamp to presence.
-	const daysDifference = Math.floor(difference / 60 / 60 / 24);
-	difference -= daysDifference * 60 * 60 * 24;
-
-	const hoursDifference = Math.floor(difference / 60 / 60);
-	difference -= hoursDifference * 60 * 60;
-
-	const minutesDifference = Math.floor(difference / 60);
-	difference -= minutesDifference * 60;
-
-	const secondsDifference = Math.floor(difference);
-
-	return `${hoursDifference >= 1 ? `${`0${hoursDifference}`.slice(-2)}:` : ""}${`0${minutesDifference}`.slice(-2)}:${`0${secondsDifference}`.slice(
-		-2
-	)} ${end ? "left" : "elapsed"}`;
-};
