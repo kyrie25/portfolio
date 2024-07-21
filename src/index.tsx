af5d9f9c7fea758ff2d85dfca4a92951546b80c3
Nam Anh <phamnamanh25@gmail.com>
2024-07-21 16:58:39 +0700

    chore(vercel): reinject script

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { createRoot } from "react-dom/client";
-import { Analytics } from "@vercel/analytics/react";
+import { inject } from "@vercel/analytics";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faDiscord, faTwitter } from "@fortawesome/free-brands-svg-icons";
@@ -16,6 +16,8 @@ import { LoadingIcon, Anchor, Image } from "./Misc";
 import { fetchAPI } from "./utils";
 
 const DISCORD_ID = import.meta.env.VITE_DISCORD_ID;
+
+inject();
 
 const App: React.FC = () => {
 	const [data, setData] = React.useState<Record<string, any>>({});
@@ -48,74 +50,71 @@ const App: React.FC = () => {
 	const loading = fetching || !avatarLoaded || !bannerLoaded || !lanyardLoaded || !onekoLoaded;
 
 	return (
-		<>
-			<main>
-				<Tooltip
-					id="tooltip"
-					style={{
-						zIndex: 9999999,
-					}}
-				/>
-				<div className={classNames("loading", { "fade-out": !loading })}>
-					<LoadingIcon />
-				</div>
-				<section>
-					<header>
-						{data.banner && (
-							<div className="banner" style={{ backgroundImage: `url(${banner})` }}>
-								<Image src={banner} alt="banner" onLoad={() => setBannerLoaded(true)} />
-								<Image src={oneko} alt="oneko" onLoad={() => setOnekoLoaded(true)} />
-							</div>
-						)}
-						<div className="avatar">
-							{data.avatar && <Image src={avatar} alt="avatar" onLoad={() => setAvatarLoaded(true)} />}
-							<div className="name">
-								<h1>Kyrie</h1>
-								<span>@kyrie25</span>
-							</div>
+		<main>
+			<Tooltip
+				id="tooltip"
+				style={{
+					zIndex: 9999999,
+				}}
+			/>
+			<div className={classNames("loading", { "fade-out": !loading })}>
+				<LoadingIcon />
+			</div>
+			<section>
+				<header>
+					{data.banner && (
+						<div className="banner" style={{ backgroundImage: `url(${banner})` }}>
+							<Image src={banner} alt="banner" onLoad={() => setBannerLoaded(true)} />
+							<Image src={oneko} alt="oneko" onLoad={() => setOnekoLoaded(true)} />
 						</div>
-					</header>
-
-					<Lanyard id={DISCORD_ID} loaded={setLanyardLoaded} />
-
-					<article>
-						<h3>
-							Junior full-stack developer, CS undergraduate at fit@<Anchor href="https://en.hcmus.edu.vn/">hcmus</Anchor>
-						</h3>
-					</article>
-
-					<article>
-						<p>Absolute Granblue nerd</p>
-					</article>
-
-					<article>
-						<img
-							id="stats"
-							src="https://github-readme-stats.vercel.app/api?username=kyrie25&include_all_commits=true&show_icons=true&count_private=true&custom_title=Kyrie%27s+GitHub+Stats&theme=react&border_color=1d2a38&bg_color=1d2a38"
-						/>
-					</article>
-
-					<article>
-						<h3>Contact me via:</h3>
-						<div className="icons">
-							<Anchor href="mailto:contact@kyrie25.me" title="Mail">
-								<FontAwesomeIcon icon={faEnvelope} size="1x" />
-							</Anchor>
-							<Anchor href={`https://discord.com/users/${data.id}`} title="Discord">
-								<FontAwesomeIcon icon={faDiscord} size="1x" />
-							</Anchor>
-							<Anchor href="https://twitter.com/_kyrie_25" title="Twitter">
-								<FontAwesomeIcon icon={faTwitter} size="1x" />
-							</Anchor>
-							<Anchor href="https://github.com/kyrie25" title="GitHub">
-								<FontAwesomeIcon icon={faGithub} size="1x" />
-							</Anchor>
+					)}
+					<div className="avatar">
+						{data.avatar && <Image src={avatar} alt="avatar" onLoad={() => setAvatarLoaded(true)} />}
+						<div className="name">
+							<h1>Kyrie</h1>
+							<span>@kyrie25</span>
 						</div>
-					</article>
-				</section>
-			</main>
-			<Analytics />
-		</>
+					</div>
+				</header>
+
+				<Lanyard id={DISCORD_ID} loaded={setLanyardLoaded} />
+
+				<article>
+					<h3>
+						Junior full-stack developer, CS undergraduate at fit@<Anchor href="https://en.hcmus.edu.vn/">hcmus</Anchor>
+					</h3>
+				</article>
+
+				<article>
+					<p>Absolute Granblue nerd</p>
+				</article>
+
+				<article>
+					<img
+						id="stats"
+						src="https://github-readme-stats.vercel.app/api?username=kyrie25&include_all_commits=true&show_icons=true&count_private=true&custom_title=Kyrie%27s+GitHub+Stats&theme=react&border_color=1d2a38&bg_color=1d2a38"
+					/>
+				</article>
+
+				<article>
+					<h3>Contact me via:</h3>
+					<div className="icons">
+						<Anchor href="mailto:contact@kyrie25.me" title="Mail">
+							<FontAwesomeIcon icon={faEnvelope} size="1x" />
+						</Anchor>
+						<Anchor href={`https://discord.com/users/${data.id}`} title="Discord">
+							<FontAwesomeIcon icon={faDiscord} size="1x" />
+						</Anchor>
+						<Anchor href="https://twitter.com/_kyrie_25" title="Twitter">
+							<FontAwesomeIcon icon={faTwitter} size="1x" />
+						</Anchor>
+						<Anchor href="https://github.com/kyrie25" title="GitHub">
+							<FontAwesomeIcon icon={faGithub} size="1x" />
+						</Anchor>
+					</div>
+				</article>
+			</section>
+		</main>
 	);
 };
 
