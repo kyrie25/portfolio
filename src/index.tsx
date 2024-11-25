4525805a67a12fde5a550faded6ddd138e6143d4
Nam Anh <phamnamanh25@gmail.com>
2024-11-25 23:50:03 +0700

    perf: reduce gif banner size

--- src/index.tsx
+++ src/index.tsx
@@ -58,7 +58,9 @@ const App: React.FC = () => {
 	const ext = (hash: string | null) => (hash?.startsWith("a_") ? "gif" : "webp");
 
 	const avatar = `https://cdn.discordapp.com/avatars/${data?.id}/${data?.avatar}.${ext(data?.avatar)}?size=256`;
-	const banner = `https://cdn.discordapp.com/banners/${data?.id}/${data?.banner}.${ext(data?.banner)}?size=2048`;
+	const banner = `https://cdn.discordapp.com/banners/${data?.id}/${data?.banner}.${ext(data?.banner)}?size=${
+		ext(data?.banner) === "gif" ? 512 : 2048
+	}`;
 	const decoration = `https://cdn.discordapp.com/avatar-decoration-presets/${data?.avatar_decoration_data?.asset}.png`;
 
 	const loading = !data || !Object.values(loadingState).every((state) => state);
